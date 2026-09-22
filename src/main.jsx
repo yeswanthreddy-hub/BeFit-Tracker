4f0b89241ce1bb7c3968e172d51e71569e9e61de
Muthyala Yeswanth Reddy <yashreddymuthyala@gmail.com>
2026-09-22 23:13:25 +0530

    style: establish BeFit design system

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles/global.css'
+import './styles/components.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
